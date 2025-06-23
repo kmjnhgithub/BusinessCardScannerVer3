bb47f3c891f94ff5134ced682458b6ffd034dfa9
Mike <kmjnh2016@gmail.com>
2025-06-23 11:35:47 +0800

    Initial Commit

---
+++ BusinessCardScannerVer3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BusinessCardScannerVer3
+//
+//  Created by mike liu on 2025/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
